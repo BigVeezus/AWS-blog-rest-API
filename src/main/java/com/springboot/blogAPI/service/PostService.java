d724ee29631b60a46fa352264e12c290fc9efb07
Elvis O <elvis.osujic@gmail.com>
2023-06-11 23:38:27 +0100

    Added GetAllPosts Requests

--- src/main/java/com/springboot/blogAPI/service/PostService.java
+++ src/main/java/com/springboot/blogAPI/service/PostService.java
@@ -2,6 +2,10 @@ package com.springboot.blogAPI.service;
 
 import com.springboot.blogAPI.dto.PostDto;
 
+import java.util.List;
+
 public interface PostService {
     PostDto createPost(PostDto postDto);
+
+    List<PostDto> getAllPosts();
 }
--- src/main/java/com/springboot/blogAPI/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blogAPI/service/impl/PostServiceImpl.java
@@ -6,6 +6,9 @@ import com.springboot.blogAPI.repository.PostRepository;
 import com.springboot.blogAPI.service.PostService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PostServiceImpl implements PostService {
 
@@ -15,24 +18,50 @@ public class PostServiceImpl implements PostService {
         this.postRepository = postRepository;
     }
 
-    @Override
-    public PostDto createPost(PostDto postDto) {
 
-        //convert DTO to entity
+    private PostDto convertToDTO(Post post){
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+
+        return postDto;
+    }
+
+    private Post convertToEntity(PostDto postDto){
         Post post = new Post();
         post.setTitle(postDto.getTitle());
         post.setDescription(postDto.getDescription());
         post.setContent(postDto.getContent());
 
+        return post;
+    }
+
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+
+        //convert DTO to entity
+        Post post = convertToEntity(postDto);
+
+        // Save new post
         Post newPost = postRepository.save(post);
 
         //convert entity to PostDTO
-        PostDto postResponse = new PostDto();
-        postResponse.setId(newPost.getId());
-        postResponse.setTitle(newPost.getTitle());
-        postResponse.setDescription(newPost.getDescription());
-        postResponse.setContent(newPost.getContent());
 
-        return postResponse;
+        return convertToDTO(newPost);
+
     }
+
+    @Override
+    public List<PostDto> getAllPosts() {
+
+        List<Post> posts = postRepository.findAll();
+        return posts.stream().map(this::convertToDTO).collect(Collectors.toList());
+
+    }
+
+
+
 }
